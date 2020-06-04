e2f6a6420f49cac861038e3e700fe5b75cdb7034
yazen firas <psyazen3@gmail.com>
2020-06-04 00:54:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(resp=> resp.json()).then(json=>renderBooks(json))
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
 
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books').then(resp=> resp.json()).then(json=>renderBooks(json))
+  .catch((err)=>{console.log(err)})
 }
 
 function renderBooks(json) {
@@ -14,6 +16,3 @@ function renderBooks(json) {
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
